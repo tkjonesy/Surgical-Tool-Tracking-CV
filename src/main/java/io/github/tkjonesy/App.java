c69b7b87eefe0cc3e6097257a38cba0672a1b5ac
Hunter Herbst <hunter.d.herbst2k@gmail.com>
2024-11-13 16:26:08 -0500

    Remove old UI code

--- src/main/java/io/github/tkjonesy/App.java
+++ src/main/java/io/github/tkjonesy/App.java
@@ -7,29 +7,12 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.ArrayList;
-import java.util.List;
-
-import io.github.tkjonesy.ONNX.Detection;
-import io.github.tkjonesy.ONNX.ImageUtil;
 import lombok.Getter;
 import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
-
-import io.github.tkjonesy.ONNX.models.OnnxOutput;
-import io.github.tkjonesy.ONNX.models.OnnxRunner;
 
 import static io.github.tkjonesy.ONNX.settings.Settings.*;
-
 
 public class App extends JFrame {
 
@@ -255,265 +238,11 @@ public class App extends JFrame {
         recLogButton.setEnabled(logEnable);
     }
 
+    private int saveConfirmationDialogue(JButton button) {
+        return JOptionPane.showConfirmDialog(button, "Save Recording?", "Save Data", JOptionPane.YES_NO_CANCEL_OPTION);
+    }
+
     public static void main(String[] args) {
-
-        // TODO remove Jake's initial UI code
-        //Initialize Frame
-        JFrame frame = new JFrame("Surgical Tool Tracker");
-        frame.setSize(960,540);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new Color(52,52,52));
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        //Panel for Camera View
-        JPanel cameraPanel = new JPanel();
-        cameraPanel.setBackground(new Color(0,0,0));
-        TitledBorder cameraBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(255,255,255)), "Camera View");
-        cameraBorder.setTitleColor(new Color(255,255,255));
-        cameraPanel.setBorder(cameraBorder);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.weightx = 0.5;
-        c.weighty = 1;
-        c.fill = GridBagConstraints.BOTH;
-        frame.add(cameraPanel, c);
-
-        //Panel for Tracking Log
-        JPanel trackingPanel = new JPanel();
-        trackingPanel.setBackground(new Color(0,0,0));
-        TitledBorder trackingBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(255,255,255)), "Tracking Log");
-        trackingBorder.setTitleColor(new Color(255,255,255));
-        trackingPanel.setBorder(trackingBorder);
-        c.gridx = 1;
-        c.gridy = 0;
-        c.weightx = 0.5;
-        c.weighty = 0.5;
-        c.fill = GridBagConstraints.BOTH;
-        frame.add(trackingPanel, c);
-
-        //Panel for Bottom Bar
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setLayout(new FlowLayout());
-        bottomPanel.setBackground(new Color(27,27,27));
-
-        //Initialize Buttons
-        JButton settingsButton = new JButton();
-        JButton recCameraButton = new JButton();
-        JButton recAllButton = new JButton();
-        JButton recLogButton = new JButton();
-
-        //Settings Button
-        settingsButton.setBounds(0, 0, 200, 50);
-        settingsButton.setText("Settings");
-        settingsButton.setFocusable(false);
-        settingsButton.setForeground(new Color(255,255,255));
-        settingsButton.setBackground(new Color(52,52,52));
-        settingsButton.setBorder(BorderFactory.createMatteBorder(5, 10, 5, 10, new Color(52,52,52)));
-        settingsButton.addActionListener(e -> System.out.println("Settings Clicked"));
-
-        //Record Camera Button
-        recCameraButton.setBounds(0, 0, 200, 50);
-        recCameraButton.setText("Start Camera");
-        recCameraButton.setFocusable(false);
-        recCameraButton.setForeground(new Color(255,255,255));
-        recCameraButton.setBackground(new Color(52,52,52));
-        recCameraButton.setBorder(BorderFactory.createMatteBorder(5, 10, 5, 10, new Color(52,52,52)));
-        recCameraButton.addActionListener(e -> {
-            if(recCameraButton.getText().equals("Start Camera")){
-                System.out.println("Start Camera Clicked");
-                recCameraButton.setText("Stop Camera");
-                //Change Borders Title
-                cameraBorder.setTitle("Camera View : Recording");
-                cameraBorder.setTitleColor(new Color(255,0,0));
-                cameraPanel.repaint();
-                //Disable Buttons
-                recAllButton.setEnabled(false);
-                recLogButton.setEnabled(false);
-            }
-            else {
-                System.out.println("Stop Camera Clicked");
-
-                int answer = JOptionPane.showConfirmDialog(recLogButton, "Save Recording?", "Save Data", JOptionPane.YES_NO_CANCEL_OPTION);
-
-                //Yes: Save Data
-                if(answer == 0) {
-                    System.out.println("Save Camera Recording Clicked\n");
-                    recCameraButton.setText("Start Camera");
-                    //Change Borders Title
-                    cameraBorder.setTitle("Camera View");
-                    cameraBorder.setTitleColor(new Color(255,255,255));
-                    cameraPanel.repaint();
-                    //Enable Buttons
-                    recAllButton.setEnabled(true);
-                    recLogButton.setEnabled(true);
-                }
-
-                //No: Don't Save Data
-                else if(answer == 1) {
-                    System.out.println("Don't Save Camera Recording Clicked\n");
-                    recCameraButton.setText("Start Camera");
-                    //Change Borders Title
-                    cameraBorder.setTitle("Camera View");
-                    cameraBorder.setTitleColor(new Color(255,255,255));
-                    cameraPanel.repaint();
-                    //Enable Buttons
-                    recAllButton.setEnabled(true);
-                    recLogButton.setEnabled(true);
-                }
-
-                //Cancel
-                else if(answer == 2) {
-                    System.out.println("Cancel Clicked");
-                }
-            }
-        });
-
-        //Record All Button
-        recAllButton.setBounds(0, 0, 200, 50);
-        recAllButton.setText("Start All");
-        recAllButton.setFocusable(false);
-        recAllButton.setForeground(new Color(255,255,255));
-        recAllButton.setBackground(new Color(52,52,52));
-        recAllButton.setBorder(BorderFactory.createMatteBorder(5, 10, 5, 10, new Color(52,52,52)));
-        recAllButton.addActionListener(e -> {
-            if(recAllButton.getText().equals("Start All")){
-                System.out.println("Start All Clicked");
-                recAllButton.setText("Stop All");
-                //Change Camera Border Title
-                cameraBorder.setTitle("Camera View : Recording");
-                cameraBorder.setTitleColor(new Color(255,0,0));
-                cameraPanel.repaint();
-                //Change Tracking Border Title
-                trackingBorder.setTitle("Tracking View : Recording");
-                trackingBorder.setTitleColor(new Color(255,0,0));
-                trackingPanel.repaint();
-                //Disable Buttons
-                recCameraButton.setEnabled(false);
-                recLogButton.setEnabled(false);
-            }
-            else {
-                System.out.println("Stop All Clicked");
-
-                int answer = JOptionPane.showConfirmDialog(recLogButton, "Save Recording?", "Save Data", JOptionPane.YES_NO_CANCEL_OPTION);
-
-                //Yes: Save Data
-                if(answer == 0) {
-                    System.out.println("Save All Recording Clicked\n");
-                    recAllButton.setText("Start All");
-                    //Change Camera Border Title
-                    cameraBorder.setTitle("Camera View");
-                    cameraBorder.setTitleColor(new Color(255,255,255));
-                    cameraPanel.repaint();
-                    //Change Tracking Border Title
-                    trackingBorder.setTitle("Tracking View");
-                    trackingBorder.setTitleColor(new Color(255,255,255));
-                    trackingPanel.repaint();
-                    //Enable Buttons
-                    recCameraButton.setEnabled(true);
-                    recLogButton.setEnabled(true);
-                }
-
-                //No: Don't Save Data
-                else if(answer == 1) {
-                    System.out.println("Don't Save All Recording Clicked\n");
-                    recAllButton.setText("Start All");
-                    //Change Camera Border Title
-                    cameraBorder.setTitle("Camera View");
-                    cameraBorder.setTitleColor(new Color(255,255,255));
-                    cameraPanel.repaint();
-                    //Change Tracking Border Title
-                    trackingBorder.setTitle("Tracking View");
-                    trackingBorder.setTitleColor(new Color(255,255,255));
-                    trackingPanel.repaint();
-                    //Enable Buttons
-                    recCameraButton.setEnabled(true);
-                    recLogButton.setEnabled(true);
-                }
-
-                //Cancel
-                else if(answer == 2) {
-                    System.out.println("Cancel Clicked");
-                }
-            }
-        });
-
-        //Record Log Button
-        recLogButton.setBounds(0, 0, 200, 50);
-        recLogButton.setText("Start Log");
-        recLogButton.setFocusable(false);
-        recLogButton.setForeground(new Color(255,255,255));
-        recLogButton.setBackground(new Color(52,52,52));
-        recLogButton.setBorder(BorderFactory.createMatteBorder(5, 10, 5, 10, new Color(52,52,52)));
-        recLogButton.addActionListener(e -> {
-            if(recLogButton.getText().equals("Start Log")){
-                System.out.println("Start Log Clicked");
-                recLogButton.setText("Stop Log");
-                //Change Tracking Border Title
-                trackingBorder.setTitle("Tracking View : Recording");
-                trackingBorder.setTitleColor(new Color(255,0,0));
-                trackingPanel.repaint();
-                //Disable Buttons
-                recCameraButton.setEnabled(false);
-                recAllButton.setEnabled(false);
-            }
-            else {
-                System.out.println("Stop Log Clicked");
-
-                int answer = JOptionPane.showConfirmDialog(recLogButton, "Save Recording?", "Save Data", JOptionPane.YES_NO_CANCEL_OPTION);
-
-                //Yes: Save Data
-                if(answer == 0) {
-                    System.out.println("Save Log Recording Clicked\n");
-                    recLogButton.setText("Start Log");
-                    //Change Tracking Border Title
-                    trackingBorder.setTitle("Tracking View");
-                    trackingBorder.setTitleColor(new Color(255,255,255));
-                    trackingPanel.repaint();
-                    //Enable buttons
-                    recCameraButton.setEnabled(true);
-                    recAllButton.setEnabled(true);
-                }
-
-                //No: Don't Save Data
-                else if(answer == 1) {
-                    System.out.println("Don't Save Log Recording Clicked\n");
-                    recLogButton.setText("Start Log");
-                    //Change Tracking Border Title
-                    trackingBorder.setTitle("Tracking View");
-                    trackingBorder.setTitleColor(new Color(255,255,255));
-                    trackingPanel.repaint();
-                    //Enable buttons
-                    recCameraButton.setEnabled(true);
-                    recAllButton.setEnabled(true);
-                }
-
-                //Cancel
-                else if(answer == 2) {
-                    System.out.println("Cancel Clicked");
-                }
-            }
-
-        });
-
-        //Add buttons to bottom panel
-        bottomPanel.add(settingsButton);
-        bottomPanel.add(recCameraButton);
-        bottomPanel.add(recAllButton);
-        bottomPanel.add(recLogButton);
-
-        //Add bottom panel
-        c.gridx = 0;
-        c.gridy = 1;
-        c.weightx = 1;
-        c.weighty = .05;
-        c.gridwidth = 2;
-        c.fill = GridBagConstraints.BOTH;
-        frame.add(bottomPanel, c);
-
-        //Display Frame
-        frame.setVisible(true);
-
         // Attempt to set L&F to system default for now
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -525,89 +254,56 @@ public class App extends JFrame {
         // Invoke instance of STT App
         SwingUtilities.invokeLater(App::new);
     }
+}
 
+/*
+recCameraButton.addActionListener(e -> {
+    if(recCameraButton.getText().equals("Start Camera")){
+        System.out.println("Start Camera Clicked");
+        recCameraButton.setText("Stop Camera");
+        //Change Borders Title
+        cameraBorder.setTitle("Camera View : Recording");
+        cameraBorder.setTitleColor(new Color(255,0,0));
+        cameraPanel.repaint();
+        //Disable Buttons
+        recAllButton.setEnabled(false);
+        recLogButton.setEnabled(false);
+    }
+    else {
+        System.out.println("Stop Camera Clicked");
 
-    private static class CameraFetcher implements Runnable {
+        int answer = JOptionPane.showConfirmDialog(recLogButton, "Save Recording?", "Save Data", JOptionPane.YES_NO_CANCEL_OPTION);
 
-        private final JLabel cameraFeed;
-        private final VideoCapture camera;
-        private final Timer timer;
-
-        public CameraFetcher(JLabel cameraFeed, VideoCapture camera) {
-            this.cameraFeed = cameraFeed;
-            this.camera = camera;
-            this.timer = new Timer();
+        //Yes: Save Data
+        if(answer == 0) {
+            System.out.println("Save Camera Recording Clicked\n");
+            recCameraButton.setText("Start Camera");
+            //Change Borders Title
+            cameraBorder.setTitle("Camera View");
+            cameraBorder.setTitleColor(new Color(255,255,255));
+            cameraPanel.repaint();
+            //Enable Buttons
+            recAllButton.setEnabled(true);
+            recLogButton.setEnabled(true);
         }
 
-        private static BufferedImage cvt2bi(Mat frame) {
-            // Select grayscale or color based on incoming frame
-            int type = BufferedImage.TYPE_BYTE_GRAY;
-            if (frame.channels() > 1) {
-                type = BufferedImage.TYPE_3BYTE_BGR;
-            }
-
-            // Create buffer to store bytes
-            int bufferSize = frame.channels() * frame.cols() * frame.rows();
-            byte[] b = new byte[bufferSize];
-
-            // Copy data from Mat into the BufferedImage
-            frame.get(0, 0, b);
-            BufferedImage image = new BufferedImage(frame.cols(), frame.rows(), type);
-            final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-            System.arraycopy(b, 0, targetPixels, 0, b.length);
-            return image;
+        //No: Don't Save Data
+        else if(answer == 1) {
+            System.out.println("Don't Save Camera Recording Clicked\n");
+            recCameraButton.setText("Start Camera");
+            //Change Borders Title
+            cameraBorder.setTitle("Camera View");
+            cameraBorder.setTitleColor(new Color(255,255,255));
+            cameraPanel.repaint();
+            //Enable Buttons
+            recAllButton.setEnabled(true);
+            recLogButton.setEnabled(true);
         }
 
-        @Override
-        public void run() {
-            // New task to run at frame rate specified in Settings file
-
-            camera.set(Videoio.CAP_PROP_FRAME_WIDTH, cameraFeed.getWidth());
-            camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, cameraFeed.getHeight());
-
-            TimerTask task = new TimerTask() {
-
-                // Required objects for detections & display
-                private static final Mat frame = new Mat();
-                private static int currentFrame = 0;
-                private static final OnnxRunner onnxRunner = new OnnxRunner();
-                private static OnnxOutput onnxOutput;
-                private static List<Detection> detections = new ArrayList<>();
-                @Override
-                public void run() {
-                    // Run if the thread hasn't been interrupted, otherwise purge the timer's schedule
-                    if(!Thread.currentThread().isInterrupted()) {
-
-                        // Resize camera size to whatever the current feed window size is
-//                        camera.set(Videoio.CAP_PROP_FRAME_WIDTH, cameraFeed.getWidth());
-//                        camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, cameraFeed.getHeight());
-                        // Pull frame and run through Onnx
-                        camera.read(frame);
-
-
-                        // Every Nth frame, we run the object detection on it
-                        if (++currentFrame % PROCESS_EVERY_NTH_FRAME == 0) {
-                            new Thread(() -> {
-                                onnxOutput = onnxRunner.runInference(frame);
-                                detections = onnxOutput.getDetectionList();
-                            }).start();
-                            currentFrame = 0;
-                        }
-
-                        // Overlay the predictions, and display the frame on the label
-                        ImageUtil.drawPredictions(frame, detections);
-                        Imgproc.resize(frame, frame, new Size(cameraFeed.getWidth(), cameraFeed.getHeight()));
-                        BufferedImage biFrame = cvt2bi(frame);
-                        cameraFeed.setIcon(new ImageIcon(biFrame));
-                    }
-                    else {
-                        timer.cancel();
-                        timer.purge();
-                    }
-                }
-            };
-            // Set the task to execute every CAMERA_FRAME_RATE frames per second
-            timer.schedule(task, 0, 1000 / CAMERA_FRAME_RATE);
+        //Cancel
+        else if(answer == 2) {
+            System.out.println("Cancel Clicked");
         }
     }
-}
+});
+* */
